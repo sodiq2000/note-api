65343dc2a6890c8ef7dca2a5f82dc58daa17f7d2
Muhammed Sodiq <muhammedsodiq2018@gmail.com>
2024-01-07 12:46:03 +0100

    small fix

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.use(cors());
 
 app.use('/', noteRoutes)
 
-const CONN_URL = process.env.CONN_URL;
+const CONN_URL =
+  process.env.CONN_URL ||
+  "mongodb+srv://primedev:prime_code123@cluster0.ken0buc.mongodb.net/notes-db?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONN_URL,{ useNewUrlParser: true, useUnifiedTopology: true})
